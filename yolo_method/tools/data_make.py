06aa19b073cca4af29982c90d8b2c0837eaa81e2
yyq90 <420936401@qq.com>
2018-06-16 12:32:00 +0800

    3

--- yolo_method/tools/data_make.py
+++ yolo_method/tools/data_make.py
@@ -7,7 +7,7 @@ make train use
 train_path = 'D:/data/datasets_fusai/datasets/train.txt'
 train_dir = 'D:/data/datasets_fusai/datasets/train/'
 
-f = open('train_use2.txt','w')
+f = open('train_use11.txt','w')
 lists = list()
 with open(train_path) as rf:
     lines = rf.readlines()
@@ -16,10 +16,10 @@ with open(train_path) as rf:
         img_name = contents[0]
         img_path = os.path.join(train_dir, img_name)
         img_label = contents[1]
-        xmin = contents[2]
-        ymin = contents[3]
-        xmax = contents[4]
-        ymax = contents[5]
+        xmin = str(abs(int(contents[2])))
+        ymin = str(abs(int(contents[3])))
+        xmax = str(abs(int(contents[4])))
+        ymax = str(abs(int(contents[5])))
         lists.append(img_path+' '+xmin+' '+ymin+' '+xmax+' '+ymax.strip()+' '+img_label.strip()+'\n')
 
 f.writelines(lists)
